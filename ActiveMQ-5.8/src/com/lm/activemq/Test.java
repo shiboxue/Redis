0e990ae1e2fe592aff41fbd87c797108a7b47e0e
shiboxue <1240613593@qq.com>
2019-03-20 10:52:17 +0800

    cxc

--- ActiveMQ-5.8/src/com/lm/activemq/Test.java
+++ ActiveMQ-5.8/src/com/lm/activemq/Test.java
@@ -2,7 +2,8 @@ package com.lm.activemq;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println();		
+		System.out.println();	
+		
 		System.out.println("ddd");
 	}
 }
