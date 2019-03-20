e137dcd1e697416b5c10f5da57c9b43576c1cdb6
shiboxue <1240613593@qq.com>
2019-03-20 10:59:04 +0800

    dd

--- ActiveMQ-5.8/src/com/lm/activemq/Test.java
+++ ActiveMQ-5.8/src/com/lm/activemq/Test.java
@@ -3,6 +3,6 @@ package com.lm.activemq;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println();			
-		System.out.println("ddd");
+		System.out.println("shiboxue");
 	}
 }
